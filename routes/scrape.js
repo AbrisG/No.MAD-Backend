a35dab0ed3faf9db5ab68b1f8b750fbe1c7fb309
Abris Gilvesy <abris.gilvesy@gmail.com>
2021-01-30 11:52:33 -0500

    scrape boilerplate added

---
+++ routes/scrape.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+const SneaksAPI = require("sneaks-api");
+const sneaks = new SneaksAPI();
+
+//getProducts(keyword, callback) takes in a keyword and returns an array of products
+sneaks.getProducts("Jordan 1 High", function (err, products) {
+  console.log(products);
+});
+
+//Product object includes styleID where you input it in the getProductPrices function
+//getProductPrices(styleID, callback) takes in a style ID and returns sneaker info including a price map and more images of the product
+sneaks.getProductPrices("FY2903", function (err, product) {
+  //console.log(product)
+});
+//getMostPopular(callback) returns an array of the current popular products curated by StockX
+sneaks.getMostPopular(function (err, products) {
+  //console.log(products)
+});
