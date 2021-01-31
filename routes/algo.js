cc053313754811c1f28599b71e129b3ccd60c291
abnik519 <aritra.banik519@gmail.com>
2021-01-30 20:17:51 -0500

    Working backend

--- routes/algo.js
+++ routes/algo.js
@@ -2,17 +2,21 @@ var express = require("express");
 var router = express.Router();
 //some database initialization here
 
+
+
 router.get("/", function (req, res, next) {
   var Datastore = require("nedb");
-  let db = new Datastore({ filename: "../database/database.db" });
+  let db = new Datastore({ filename: "./database/database.db" });
   db.loadDatabase();
   db.find({}, function (err, docs) {
     console.log("find succeeded");
+    res.send(docs);
     console.log(docs);
     if (err) {
       return console.error(err);
     }
   });
+  
 });
 
 module.exports = router;
