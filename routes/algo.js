1365f45a7939d88f1842372f6bde877dabd4e9b1
Abris Gilvesy <abris.gilvesy@gmail.com>
2021-02-01 21:52:44 -0500

    fixed up routes to reduce redundant code

--- routes/algo.js
+++ routes/algo.js
@@ -3,15 +3,7 @@ var router = express.Router();
 const shoeModel = require("../models/shoe");
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://AbrisG:Horse123@cluster0.0ds2t.mongodb.net/nomad?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
-);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
+mongoose.connection.once("open", function () {
   router.get("/", function (req, res) {
     shoeModel.find({}, function (err, shoes) {
       if (err) return console.error(err);
